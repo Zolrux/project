7b94d8583edfc92e5f5b8545c965d483d4d45166
Kostya <kirov1607@gmail.com>
2021-08-04 22:42:02 +0300

    DOM starting

--- script.js
+++ script.js
@@ -1,37 +1,23 @@
 "use strict";
 
-//To String
+const box = document.getElementById('box');
 
-console.log(String(null));
-console.log(typeof (null + ''));
+console.log(box);
 
-const num = 5;
+const btns = document.getElementsByTagName('button');
 
-console.log("https://vk.com//catalog/" + num);
+console.log(btns[0]);
 
-const fontSize = 26 + 'px';
+const circles = document.getElementsByClassName('circle');
+console.log(circles);
 
-// To Number
+const hearts = document.querySelectorAll('.heart');
 
-console.log(typeof (Number('4')));
+console.log(hearts);
 
-console.log(typeof (+'5'));
+hearts.forEach(item => {
+    console.log(item);
+});
 
-console.log(typeof (parseInt("15px", 10)));
-
-let answer = +prompt("Hello", "");
-
-// To boolean
-
-let switcher = null; // Null это false
-
-switcher = 1;
-
-if (switcher) {
-    console.log("Working...");
-}
-
-console.log(typeof(Boolean('4')));
-
-console.log(typeof (!!"444444")); // boolean
-
+const oneHeart = document.querySelector('.heart');
+console.log(oneHeart);
