fc66df8e99e8beb49462e077bb4862cf0af046f8
Kostya <kirov1607@gmail.com>
2021-08-05 22:14:46 +0300

    EventListeners

--- script.js
+++ script.js
@@ -1,55 +1,38 @@
 "use strict";
 
-const box = document.getElementById('box'),
-    btns = document.getElementsByTagName('button'),
-    circles = document.getElementsByClassName('circle'),
-    wrapper = document.querySelector('.wrapper'),
-    hearts = wrapper.querySelectorAll('.heart'),
-    oneHeart = wrapper.querySelector('.heart');
 
-//  box.style.backgroundColor = 'blue';
-//  box.style.width = '500px';
+const btns = document.querySelectorAll('button'),
+      overlay = document.querySelector('.overlay');
 
-box.style.cssText = `background-color: blue; width: 500px`;
+// btn.onclick = function() {
+//     alert('Click');
+// };
 
-btns[1].style.borderRadius = '100%';
-circles[0].style.backgroundColor = 'red';
+// btn.onclick = function() {
+//     alert('Second click');
+// };
 
-//  for(let i = 0; i < hearts.length; i++) {
-//      hearts[i].style.backgroundColor = 'blue';
-//  }
+// let i = 0;
+const deleteElement = (e) => {
+    console.log(e.target);
+    console.log(e.type);
+    // i++;
+    // if(i == 1) {
+    //     btn.removeEventListener('click', deleteElement);
+    // }
+};
 
-hearts.forEach(item => {
-    item.style.backgroundColor = 'blue';
+// btn.addEventListener('click', deleteElement);
+// overlay.addEventListener('click', deleteElement);
+
+btns.forEach(btn => {
+    btn.addEventListener('click', deleteElement, {once: true});
 });
 
-const div = document.createElement('div'); // Только внутри js!
-// const text = document.createTextNode('Тут был я');
+const link = document.querySelector('a');
 
-div.classList.add('black');
+link.addEventListener('click', function(e) {
+    e.preventDefault();
 
-wrapper.append(div); // В КОНЕЦ (body и тд) добавляем переменную div
-
-// wrapper.appendChild(div); Более устаревшая версия append()
-
-// wrapper.prepend(div);  В НАЧАЛО (body и тд) добавляем переменную div
-
-// hearts[0].before(div); Вставляет переменную div ПЕРЕД первым элементом
-
-// hearts[0].after(div); Вставляет переменную div ПОСЛЕ первым элементом
-
-// wrapper.insertBefore(div, hearts[1]);
-
-// circles[0].remove(); Удаляет элемент со страницы
-
-// wrapper.removeChild(hearts[1]); 
-
-// hearts[0].replaceWith(circles[0]); ЗАМЕНЯЕТ элемент на тот который был передан
-
-// wrapper.replaceChild(circles[0], hearts[0]);
-
-div.innerHTML = "<h1>Hello World</h1>";
-
-// div.textContent = "<h1>Hello World</h1>";
-
-div.insertAdjacentHTML("afterend", '<h2>Hello</h2>');
+    console.log(e.target);
+});
