e02db6a617c1a810c482a40dfeffcf248ba2e565
Kostya <kirov1607@gmail.com>
2021-08-07 12:57:18 +0300

    async and defer

--- script.js
+++ script.js
@@ -1,34 +1,13 @@
 "use strict";
 
-// touchstart
-// touchend
-// touchmove
-// touchenter
-// touchleave
-// touchcancel
+const p = document.querySelectorAll('p');
+console.log(p);
 
-window.addEventListener('DOMContentLoaded', () => {
-    const box = document.querySelector('.box');
-    box.addEventListener('touchstart', (e) => {
-        e.preventDefault();
+function loadScript(src) {
+    const script = document.createElement('script');
+    script.src = src;
+    script.async = false;
+    document.body.append(script);
+}
 
-        console.log("Start");
-        console.log(e.targetTouches);
-    });
-
-    box.addEventListener('touchmove', (e) => {
-        e.preventDefault();
-
-        console.log(e.targetTouches[0].pageX);
-    });
-
-    // box.addEventListener('touchend', (e) => {
-    //     e.preventDefault();
-
-    //     console.log("End");
-    // });
-}); 
-
-// touches
-// targetTouches
-// changedTouches
+loadScript("test.js");
