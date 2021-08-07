c88d123ab9fa4bbe8fc3021a9552a58d422a5529
Kostya <kirov1607@gmail.com>
2021-08-07 12:39:25 +0300

    EventListeners on touchpad devices

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
 "use strict";
 
-// console.log(document.head);
-// console.log(document.documentElement);
-// console.log(document.body.childNodes);
-// console.log(document.body.firstElementChild);
-// console.log(document.body.lastChild);
+// touchstart
+// touchend
+// touchmove
+// touchenter
+// touchleave
+// touchcancel
 
-// console.log(document.querySelector('#current').parentNode);
-// console.log(document.querySelector('#current').parentElement);
+window.addEventListener('DOMContentLoaded', () => {
+    const box = document.querySelector('.box');
+    box.addEventListener('touchstart', (e) => {
+        e.preventDefault();
 
-// console.log(document.querySelector('[data-current="3"]').nextElementSibling);
+        console.log("Start");
+        console.log(e.targetTouches);
+    });
 
-for (let node of document.body.childNodes) {
-    if( node.nodeName == '#text') {
-        continue;
-    }
+    box.addEventListener('touchmove', (e) => {
+        e.preventDefault();
 
-    console.log(node);
-}
+        console.log(e.targetTouches[0].pageX);
+    });
+
+    // box.addEventListener('touchend', (e) => {
+    //     e.preventDefault();
+
+    //     console.log("End");
+    // });
+}); 
+
+// touches
+// targetTouches
+// changedTouches
