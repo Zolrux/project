0ba6bca7900bfee85d41156cf33c5591d82d3214
Kostya <kirov1607@gmail.com>
2021-08-16 20:24:43 +0300

    this

--- script.js
+++ script.js
@@ -1,28 +1,84 @@
 "use strict";
 
-// const num = new Function(3);
-// console.log(num);
+// function showThis(a, b) {
+//     console.log(this);
+//     function sum() {
+//         console.log(this);
+//         return a + b;
+//     }
 
-function User(name, id) {
-    this.name = name;
-    this.id = id;
-    this.human = true;
-    this.hello = function () {
-        console.log(`Hello ${this.name}`);
-    };
-}
+//     console.log(sum());
+// }
 
-User.prototype.exit = function () {
-    console.log(`Пользователь ${this.name} ушел`)
-}
+// showThis(4, 5);
 
-const kostya = new User('Kostya', 18);
-const alex = new User('Alex', 20);
+// const obj = {
+//     a: 20,
+//     b: 15,
+//     sum: function () {
+//         function shout() {
+//             console.log(this);
+//         }
+//         shout();
+//     }
+// };
 
-kostya.exit();
+// obj.sum();
 
-kostya.hello();
-alex.hello();
+// function User(name, id) {
+//     this.name = name;
+//     this.id = id;
+//     this.human = true;
+//     this.hello = function () {
+//         console.log("Hello! " + this.name);
+//     }
+// }
+// let kostya = new User('Kostya', 18);
 
-console.log(kostya);
-console.log(alex);
+// function sayName(surname) {
+//     console.log(this);
+//     console.log(this.name + surname);
+// }
+
+// const user = {
+//     name: 'John'
+// };
+
+// sayName.call(user, 'Smith');
+// sayName.apply(user, ['Smith']);
+
+// function count(num) {
+//     return this*num;
+// }
+
+// const double = count.bind(2);
+// console.log(double(3));
+// console.log(double(13));
+
+// 1) Обычная функция: this - window, но если use strict - undefined
+// 2) Контекст у методов обьекта - сам обьект
+// 3) this в конструкторах и классах - это новый экземляр обьекта
+// 4) Ручна привязка this: call, apply, bind
+
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', (e) => {
+    e.target.style.backgroundColor = 'red';
+});
+
+const obj = {
+    num: 5,
+    sayNumber: function () {
+        const say = () => {
+            console.log(this.num);
+        };
+
+        say();
+    }
+};
+
+obj.sayNumber();
+
+const double = (a, b) => a * 2;
+
+console.log(double(4));
