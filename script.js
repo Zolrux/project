f935f7d99da098dbc459b1d1c45eaa7c5d37a31f
Kostya <kirov1607@gmail.com>
2021-08-17 16:14:34 +0300

    Rest operator

--- script.js
+++ script.js
@@ -1,35 +1,13 @@
 "use strict";
 
-class Rectangle {
-    constructor(height, width) {
-        this.height = height;
-        this.width = width;
-    }
+const log = function (a, b, ...rest) {
+    console.log(a, b, rest);
+};
 
-    calcArea() {
-        return this.height * this.width;
-    }
+log('basic', 'rest', 'operator', 'usage');
+
+function calcOrDouble(number, basis = 2) {
+    console.log(number * basis);
 }
 
-class ColoredRectangleWithText extends Rectangle {
-    constructor(height, width, text, bgColor) {
-        super(height, width); // Всегда на первом месте!
-        this.text = text;
-        this.bgColor = bgColor;
-    }
-
-    showMyProps() {
-        console.log(`Текст: ${this.text}, цвет: ${this.bgColor}`);
-    }
-}
-
-const div = new ColoredRectangleWithText(25, 10, 'Hello World', 'red');
-
-div.showMyProps();
-console.log(div.calcArea());
-
-// const square = new Rectangle(10, 10);
-// const long = new Rectangle(20, 100);
-
-// console.log(square.calcArea());
-// console.log(long.calcArea()); 
+calcOrDouble(3);
