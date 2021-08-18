aae29a86ce1d79eb668cd17999ddec4f4d4b3a5f
Kostya <kirov1607@gmail.com>
2021-08-18 19:19:06 +0300

    ajax

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 "use strict";
 
-const person = {
-    name: 'Alex',
-    tel: '+744444444',
-    parents: {
-        mom: 'Tanya',
-        dad: 'Igor'
-    }
-};
+const inputRub = document.querySelector('#rub'),
+      inputUsd = document.querySelector('#usd');
+    
+inputRub.addEventListener('input', () => {
+    const request = new XMLHttpRequest();
+    
+    request.open('GET', 'current.json');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.send();
 
-// console.log(JSON.parse(JSON.stringify(person)));
-const clone = JSON.parse(JSON.stringify(person));
-clone.parents.mom = 'Ann';
-console.log(person);
-console.log(clone);
+    request.addEventListener('load', () => {
+        if (request.status === 200) {
+            const data = JSON.parse(request.response);
+            inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
+        } else {
+            inputUsd.value = "Error! Cannot convert rub in usd";
+        }
+    });
+
+    // status
+    // statusText
+    // response
+    // readyState
+
+
+});
